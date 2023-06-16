5d7fbd495c3ce470a612c3b55bd9e76fc2612917
LeSeulMano <64015308+LeSeulMano@users.noreply.github.com>
2023-06-16 16:53:07 +0200

    Update init_bdd.py

--- site/lib/init_bdd.py
+++ site/lib/init_bdd.py
@@ -1,3 +1,3 @@
 import psycopg2
 
-con = psycopg2.connect(host='localhost', user='management_game', password='lolmdr1234', dbname='jeu')
+con = psycopg2.connect(host='', user='', password='', dbname='')
